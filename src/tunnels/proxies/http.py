d487abfb59fcc3a58b892894355900c2c4eed103
Etienne Perot <etienne@perot.me>
2013-04-30 17:51:52 -0400

    Implement HTTP proxy support.

--- src/tunnels/proxies/http.py
+++ src/tunnels/proxies/http.py
@@ -2,45 +2,45 @@ import socket as _socket
 from tunnels.proxy import Proxy as _Proxy
 from tunnels.proxy import ForwarderProxyThread as _ForwarderProxyThread
 from tunnels.proxies.socksipy import socksocket as _socksocket
-from tunnels.proxies.socksipy import PROXY_TYPE_SOCKS5 as _PROXY_TYPE_SOCKS5
+from tunnels.proxies.socksipy import PROXY_TYPE_HTTP as _PROXY_TYPE_HTTP
 
-class Socks5TCPProxyThread(_ForwarderProxyThread):
+class HTTPTCPProxyThread(_ForwarderProxyThread):
 	def _mkOutgoingSocket(self):
 		outgoingSocket = _socksocket(_socket.AF_INET, _socket.SOCK_STREAM)
-		outgoingSocket.setproxy(_PROXY_TYPE_SOCKS5, self.getParentProxy().getProxyAddress(), self.getParentProxy().getProxyPort())
+		outgoingSocket.setproxy(_PROXY_TYPE_HTTP, self.getParentProxy().getProxyAddress(), self.getParentProxy().getProxyPort())
 		outgoingSocket.connect(self.getDestination())
 		return outgoingSocket
 
-class Socks5Proxy(_Proxy):
+class HTTPProxy(_Proxy):
 	def __init__(self, name, config):
 		_Proxy.__init__(self, name, config)
 		self._proxyAddress = self['address']
 		self._proxyPort = self['port']
-		# TODO Socks auth
+		# HTTP auth?
 	def getProxyAddress(self):
 		return self._proxyAddress
 	def getProxyPort(self):
 		return self._proxyPort
 	def _getTCPThreadClass(self):
-		return Socks5TCPProxyThread
+		return HTTPTCPProxyThread
 
 proxyInfo = {
-	'class': Socks5Proxy,
+	'class': HTTPProxy,
 	'config': {
 		'address': {
-			'description': u'The address of the SOCKS server.'
+			'description': u'The address of the HTTP proxy server.'
 		},
 		'port': {
-			'default': 1080,
-			'description': u'Port number of the SOCKS server.'
+			'default': 80,
+			'description': u'Port number of the HTTP proxy server.'
 		},
 		'username': {
 			'default': None,
-			'description': u'Username to log in to the SOCKS server.'
+			'description': u'Username used for HTTP auth.'
 		},
 		'password': {
 			'default': None,
-			'description': u'Password to log in to the SOCKS server.'
+			'description': u'Password used for HTTP auth.'
 		}
 	}
 }
